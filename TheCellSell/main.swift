bbdf38190ed1392a0e399122a73c45a69741676b
Vincent He <vincent0710@Outlook.com>
2020-10-04 15:47:47 -0400

    First some what working version of the KMean cluster algorithm (BUG: Division Zero)

--- TheCellSell/main.swift
+++ TheCellSell/main.swift
@@ -14,6 +14,27 @@ struct MathFunction{
     func distance(v1 : Vector, v2: Vector) -> Double {
         return sqrt(pow(v1.vet[0]-v2.vet[0], 2) + pow(v1.vet[1]-v2.vet[1], 2))
     }
+    
+    func find_max(v1: [Double]) -> Int{
+        var maxindex = 0
+        for index in 0...(v1.count - 1){
+            if v1[maxindex] < v1[index]{
+                maxindex = index
+            }
+        }
+        return maxindex
+    }
+    
+    func count_int(findingelement: Int, list: [Int]) -> Int{
+        var count:Int = 0
+        for element in list{
+            if element == findingelement{
+                count += 1
+            }
+        }
+        return count
+    }
+    
 }
 
 
@@ -38,6 +59,9 @@ class Vector{
         //        for index in 0...(vet.vet.count - 1){
         //            results.append(self.vet[index] + vet.vet[index])
         //        }
+        if vet.vet.count != 2{
+            Swift.print("Warning this is not right!!! ")
+        }
         
         return Vector(vets: [vet.vet[0] + self.vet[0], vet.vet[1] + self.vet[1]])
     }
@@ -63,33 +87,114 @@ class Vector{
             Swift.print(element)
         }
     }
+    
+    func equal(v1: [Double]){
+        self.vet = v1
+    }
+    
+    func equal(p1 : Vector){
+        // Working on that
+    }
+    
+    func divide(k: Double){
+        var state = true
+        for element in self.vet{
+            if element == 0{
+                Swift.print("Warning There is zero")
+                state = false
+            }
+        }
+        if k == 0{
+            state = false
+            Swift.print("Warning Division warning")
+        }
+        if state{
+            for index in 0...(self.vet.count - 1){
+                self.vet[index] = self.vet[index] / k
+            }
+        }
+    }
+    
+    func divide(k: Int){
+        var state = true
+        for element in self.vet{
+            if element == 0{
+                Swift.print("Warning There is zero")
+                state = false
+            }
+        }
+        if k == 0{
+            state = false
+            Swift.print("Warning Division warning")
+        }
+        if state{
+            for index in 0...(self.vet.count - 1){
+                self.vet[index] = self.vet[index] / Double(k) // remove this in future (slow program)
+            }
+        }
+    }
 }
 
 class KMeans{
     private var data : [Vector]
-    private var centroid =  [Vector]()
-    private var numcen : Int
+    private var centroids =  [Vector]()
+    private var functions = MathFunction()
     
     init(data: [Vector], numCen: Int){
         self.data = data
-        self.numcen = numCen
-        for _ in 0...(numCen){
-            self.centroid.append(Vector(vets: [Double.random(in: 0...1), Double.random(in: 0...1)])) // Generate a random centriod
+        for _ in 0...(numCen - 1){
+            self.centroids.append(Vector(vets: [Double.random(in: 0...1), Double.random(in: 0...1)])) // Generate a random centriod
         }
     }
     
-    func fit(){
+    func fit(epochs: Int) {
         // This would start to compute the kmena algorithm
-        Swift.print("Hello darkness")
+        
+        for _ in 0...(epochs - 1){
+            // Part One Finding the cloest centriod
+            var indexs = [Int]()
+            
+            for data in self.data{
+                var distances = [Double]()
+                
+                for centriod in self.centroids{
+                    distances.append(self.functions.distance(v1: data, v2:centriod))
+                }
+                indexs.append(self.functions.find_max(v1:distances))
+            }
+            
+            // Part Two computing the new centriod of kmean
+            
+            for index in 0...(self.centroids.count - 1){
+                self.centroids[index] = Vector(vets: [0.0, 0.0])
+            }
+            
+            for index in 0...(self.data.count - 1){
+                self.centroids[indexs[index]] = self.centroids[indexs[index]].add (vet: self.data[index] )
+            }
+            
+            // The division part of the algorithm
+            for index in 0...(self.centroids.count - 1){
+                self.centroids[index].divide(
+                    k:self.functions.count_int(findingelement: index, list: indexs)
+                )
+            }
+            
+            // Printing the vectors
+            Swift.print("Result===========")
+            for vector in self.centroids{
+                vector.print()
+            }
+            Swift.print("Result===========")
+        }
     }
-    
-    
 }
 
 var data = [Vector]()
-for _ in 0...100{
-    data.append(Vector(vets: [1.1, 2.2]))
+for _ in 0...100000{
+    data.append(Vector(vets: [Double.random(in: -100...100), Double.random(in: -100...100)] ))
 }
 
-var kmean = KMeans(data: data, numCen: 3)
-kmean.fit()
+var kmean = KMeans(data: data, numCen: 4)
+kmean.fit(epochs: 100)
+
